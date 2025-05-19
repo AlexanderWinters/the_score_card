9814763f2a8c77bac2ed1577e53453c256199fa5
alex <alex@kknds.com>
2025-05-19 10:03:32 +0200

    "Fix stylesheet import case in SignupPage and LoginPage"
    Signed-off-by: alex <alex@kknds.com>

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { generateUserKey, registerUser } from '../api/authApi';
 import { useAuth } from './AuthContext';
-import '../styles/LoginPage.css';
+import '../styles/loginPage.css';
 
 function SignupPage() {
     const [userKey, setUserKey] = useState('');
